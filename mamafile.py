76fadfeeaf9cfe7e8f62c764b08d036e52468cce
Jorma Rebane <jorma.rebane@gmail.com>
2023-10-22 00:03:08 +0300

    went full static due to linking issues and smaller fw image

--- mamafile.py
+++ mamafile.py
@@ -11,9 +11,8 @@ class iptables(mama.BuildTarget):
         self.libmnl = self.gnu_project('libmnl', '1.0.5',
             url='https://kratt.codefox.ee/linux/{{project}}.tar.bz2',
             build_products=[
-                BuildProduct('{{installed}}/lib/libmnl.so.0.2.0', None),
+                BuildProduct('{{installed}}/lib/libmnl.a', None),
             ])
-        
         self.iptables = self.gnu_project('iptables', '1.8.9',
             url='https://kratt.codefox.ee/linux/{{project}}.tar.xz',
             build_products=[
@@ -29,14 +28,14 @@ class iptables(mama.BuildTarget):
 
     def build(self):
         if self.libmnl.should_build():
-            self.libmnl.build(options='--disable-static --enable-shared', multithreaded=True)
+            self.libmnl.build(options='--enable-static', multithreaded=True)
 
         if self.iptables.should_build():
             iptables_opts = '--with-gnu-ld --disable-ipv6 --disable-nftables'
             iptables_opts += ' --enable-silent-rules --enable-devel'
             iptables_opts += ' --disable-shared --enable-static' # build everything as static
             self.iptables.extra_env['libmnl_CFLAGS'] = f"-I{self.libmnl.install_dir('include')} "
-            self.iptables.extra_env['libmnl_LIBS'] = self.libmnl.install_dir('lib/libmnl.so')
+            self.iptables.extra_env['libmnl_LIBS'] = self.libmnl.install_dir('lib/libmnl.a')
             self.iptables.build(options=iptables_opts, multithreaded=True)
         else:
             console('sbin/iptables already built', color='green')
@@ -46,7 +45,3 @@ class iptables(mama.BuildTarget):
         self.export_include('iptables-built/include', build_dir=True)
         self.export_lib('iptables-built/lib/libxtables.a', build_dir=True)
         self.export_lib('iptables-built/lib/libip4tc.a', build_dir=True)
-
-        # TODO: might need to create a separate package for libmnl
-        self.export_include('libmnl-built/include', build_dir=True)
-        self.export_lib('libmnl-built/lib/libmnl.so', build_dir=True)
